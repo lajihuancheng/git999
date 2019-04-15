364adf02164224296185503caafbeffb743fb19e
liubo <liubo@icibei.com>
2019-04-15 18:40:20 +0800

    1

---
+++ src/M.java
@@ -0,0 +1,17 @@
+/**
+ * Description
+ * ProjectName ggggit
+ * Created by liubo on 2019/4/15 18:39.
+ * Version 1.0
+ */
+
+/**
+ *
+ * @刘波
+ * @create 2019-04-15 18:39
+ **/
+public class M {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
