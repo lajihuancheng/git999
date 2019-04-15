b4eeceadaf4d97a287d2b12c0d02b219243cdc92
liubo <liubo@icibei.com>
2019-04-15 19:15:28 +0800

    ab

---
+++ src/B.java
@@ -0,0 +1,17 @@
+/**
+ * Description
+ * ProjectName ggggit
+ * Created by liubo on 2019/4/15 19:15.
+ * Version 1.0
+ */
+
+/**
+ *
+ * @刘波
+ * @create 2019-04-15 19:15
+ **/
+public class B {
+    public static void main(String[] args) {
+
+    }
+}
