02e59dfb1d1900f313078134d4eb890b33a7c253
liubo <liubo@icibei.com>
2019-04-26 11:22:49 +0800

    System.out.println("合并2");

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello World5!");
         System.out.println("Hello World6!");
         System.out.println("Hello World7!");
+        System.out.println("Hello World8!");
     }
 }
