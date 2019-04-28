a63f6686aa6db9260caff2ed050b0b327d7d5b68
liubo <liubo@icibei.com>
2019-04-28 17:30:32 +0800

    Squashed commit of the following:
    commit f35d0083e0d28814906ce5bb8753b502b2b1f778
    Author: liubo <liubo@icibei.com>
    Date:   Sun Apr 28 17:24:59 2019 +0800
    
        asd

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println();
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
